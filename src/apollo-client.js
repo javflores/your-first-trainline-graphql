4090c924adad03be7cb8d87b3eba358a88555968
Juan Flores <juanantoniovicaria@gmail.com>
2018-07-03 18:36:37 +0100

    Update apollo-client.js

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,26 +1,9 @@
 import { ApolloClient } from 'apollo-client';
-import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const AuthLink = (operation, next) => {
-  operation.setContext(context => ({
-    ...context,
-    headers: {
-      ...context.headers
-    },
-  }));
-  
-  return next(operation);
-};
-
-const link = ApolloLink.from([
-  AuthLink,
-  new HttpLink({ uri: '/graphql' }),
-]);
-
 const apolloClient = new ApolloClient({
-  link,
+  link: new HttpLink({ uri: 'http://localhost:9000/graphql' }),
   cache: new InMemoryCache(),
 });
 
