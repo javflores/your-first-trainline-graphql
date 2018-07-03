ebc1ae3ca3a00f4d22a84145ec98af397e96269f
Juan Vicaria <juan.vicaria@thetrainline.com>
2018-07-03 20:34:45 +0100

    Plug in graphql server to React app

--- server/graphql/departing-services/index.js
+++ server/graphql/departing-services/index.js
@@ -1,11 +1,17 @@
 const client = require('superagent');
 const stations = require('../../../stations.json');
 
+const ON_TIME = 'On time';
+const DELAYED = 'Delayed';
+
 const departingServicesTypes = `
   type DepartingService {
     origin: String
     destination: String
     operator: String
+    scheduledTime: String
+    platform: Int
+    realTimeUpdate: String
   }
 `;
 
@@ -17,7 +23,10 @@ const getDepartingServicesResolver = async ({origin = "WAT"}) => {
     return {
       origin: mapToFullName(origin),
       destination: mapToFullName(readDestination(service)),
-      operator: service.serviceOperator
+      operator: service.serviceOperator,
+      scheduledTime: readScheduledTime(service),
+      platform: readPlatform(service),
+      realTimeUpdate: produceRealTimeUpdate(service)
     };
   });
 };
@@ -26,8 +35,48 @@ const readDestination = (service) => {
   return service.destinationList[0].crs;
 };
 
-function mapToFullName(code){
+const mapToFullName = (code) => {
   return stations.stations.find(x => x.crs === code).name;
+}
+
+const readScheduledTime = (service) => {
+  const scheduledTime = new Date(service.scheduledInfo.scheduledTime);
+  return toHoursAndMinutes(scheduledTime);
+}
+
+function containsRealTimePlatform(service){
+  return service.realTimeUpdatesInfo && service.realTimeUpdatesInfo.realTimeServiceInfo && service.realTimeUpdatesInfo.realTimeServiceInfo.realTimePlatform;
+}
+
+function produceRealTimeUpdate(service) {
+  if(!service.realTimeUpdatesInfo){
+    return ON_TIME;
+  }
+  
+  const delayReason = service.realTimeUpdatesInfo.delayReason;
+  if(delayReason) {
+    return DELAYED;
+  }
+  
+  if(!service.realTimeUpdatesInfo.realTimeServiceInfo){
+    return ON_TIME;
+  }
+  
+  const realTime = new Date(service.realTimeUpdatesInfo.realTimeServiceInfo.realTime);
+  const scheduleTime = new Date(service.scheduledInfo.scheduledTime);
+  return realTime > scheduleTime ? toHoursAndMinutes(realTime) : ON_TIME;
+}
+
+function toHoursAndMinutes(date){
+  return date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+}
+
+function readPlatform(service){
+  if(containsRealTimePlatform(service)){
+    return Number(service.realTimeUpdatesInfo.realTimeServiceInfo.realTimePlatform);
+  }
+  
+  return Number(service.scheduledInfo.scheduledPlatform);
 }
 
 const departingServicesResolvers = {
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { graphiqlExpress } = require('apollo-server-express');
+const cors = require('cors');
 
 const graphql = require('./graphql');
 
 const PORT = process.env.PORT || 9000;
 const app = express();
 
+app.use(cors());
 app.use('/graphql', graphql);
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
