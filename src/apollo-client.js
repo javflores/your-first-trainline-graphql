6d40eadd43620e4928df8d2fb21ad685fdf5af99
Juan Vicaria <juan.vicaria@thetrainline.com>
2018-07-04 20:10:09 +0100

    Client side setup

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -1,9 +1,30 @@
+import { WebSocketLink } from 'apollo-link-ws';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:9000/graphql' });
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:9000/subscriptions`,
+  options: {
+    reconnect: true
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
 
 const apolloClient = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:9000/graphql' }),
+  link: link,
   cache: new InMemoryCache(),
 });
 
